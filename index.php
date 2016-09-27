6bb4ee1698d08c5de760e6c9665cfc10b7e6d182
Lewis Goddard <lewisgoddard@eustasy.org>
2016-09-28 00:07:35 +0100

    Move to templated

--- index.php
+++ index.php
@@ -1,109 +1,85 @@
-<?php include 'header.php'; ?>
-				<h1>eustasy Labs</h1>
-				<h5>by <a href="http://eustasy.org/" class="inlink">eustasy.org</a></h5>
-			</header>
-		</div>
-		<div id="maincontentcontainer">
-			<div id="maincontent">
+<?php include __DIR__.'/_templates/header.php'; ?>
 
+<header>
+	<h1>eustasy Labs</h1>
+	<p class="sub-title">by <a href="http://eustasy.org/">eustasy.org</a></p>
+</header>
 
+<hr class="section-breaker">
 
-				<div class="section group">
-					<div class="col span_6_of_12">
-						<a href="http://labs.eustasy.org/comments-evolved" title="Comments Evolved">
-							<img alt="Comments Evolved" src="http://labs.eustasy.org/comments-evolved.png">
-						</a>
-					</div>
-					<div class="col span_1_of_12"><br></div>
-					<div class="col span_5_of_12">
-						<h2>Comments Evolved</h2>
-						<p>Comments Evolved, or Comments Evolved for Websites, is based on <a href="https://github.com/CloudHeroDevOps/comments-evolved">Comments Evolved for WordPress</a>, and allows for the use of Disqus, Facebook and Google+ comments in a stylish, modular, tabbed format at the bottom of each page.</p>
-						<p class="right"><a href="http://labs.eustasy.org/comments-evolved">Demo</a> &nbsp;&middot;&nbsp; <a href="https://github.com/eustasy/comments-evolved">GitHub</a></p>
-					</div>
-				</div>
-				<br><br><br>
-				<div class="section group">
-					<div class="col span_5_of_12">
-						<h2>elementary Search</h2>
-						<p>elementary Search is a small page that allows you to search with Google, Yahoo! or DuckDuckGo in a privacy orientated environment (it uses no Cookies by default) that is both simple to use and beautiful to behold. In the future we hope to have a tighter integration with the elementary project as it moves towards Isis.</p>
-						<p class="right"><a href="http://labs.eustasy.org/esearch">Demo</a> &nbsp;&middot;&nbsp; <a href="https://github.com/eustasy/elementary-Search">GitHub</a></p>
-					</div>
-					<div class="col span_1_of_12"><br></div>
-					<div class="col span_6_of_12">
-						<a href="http://labs.eustasy.org/esearch" title="elementary Search">
-							<img alt="elementary Search" src="http://labs.eustasy.org/elementary-search.png">
-						</a>
-					</div>
-				</div>
-				<br><br><br>
-				<div class="section group">
-					<div class="col span_6_of_12">
-						<a href="http://labs.eustasy.org/jquery.equalize" title="jQuery.equalize">
-							<img alt="jQuery.equalize" src="http://labs.eustasy.org/jquery-equalize.png">
-						</a>
-					</div>
-					<div class="col span_1_of_12"><br></div>
-					<div class="col span_5_of_12">
-						<h2>jQuery.equalize</h2>
-						<p>jQuery.equalize is a small jQuery plugin to equalize the height of elements with the equalize class within groups, or the entire page. It is open-sourced by eustasy under the MIT License, and the minified version is only 1.8KB in size.</p>
-						<p class="right"><a href="http://labs.eustasy.org/jquery.equalize">Demo</a> &nbsp;&middot;&nbsp; <a href="https://github.com/eustasy/jquery.equalize">GitHub</a></p>
-					</div>
-				</div>
-				<br><br><br>
-				<div class="section group">
-					<div class="col span_5_of_12">
-						<h2>Uptime Robot Status Page</h2>
-						<p>A Status page for Uptime Reports from <a href="http://uptimerobot.com">UptimeRobot.com</a>, making only slight use of their API.</p>
-						<p class="right"><a href="http://labs.eustasy.org/labs-uptimerobot-status-page">Demo</a> &nbsp;&middot;&nbsp; <a href="https://github.com/eustasy/labs-uptimerobot-status-page">GitHub</a></p>
-					</div>
-					<div class="col span_1_of_12"><br></div>
-					<div class="col span_6_of_12">
-						<a href="http://labs.eustasy.org/labs-uptimerobot-status-page" title="Uptime Robot Status Page">
-							<img alt="Uptime Robot Status Page" src="http://labs.eustasy.org/labs_uptime-robot-status-page.png">
-						</a>
-					</div>
-				</div>
-				<br><br><br>
-				<div class="section group">
-					<div class="col span_6_of_12">
-						<a href="http://labs.eustasy.org/labs-browser-size/size.html" title="Browser Size">
-							<img alt="Browser Size" src="http://labs.eustasy.org/labs_browser-size.png">
-						</a>
-					</div>
-					<div class="col span_1_of_12"><br></div>
-					<div class="col span_5_of_12">
-						<h2>Browser Size</h2>
-						<p>Browser Size is a small HTML document that relies only on jQuery to quickly and simply display the dimensions of your browser window. This can be especially useful to have locally stored on your machine for manipulating CSS Media Queries and emulating smaller screens.</p>
-						<p class="right"><a href="http://labs.eustasy.org/labs-browser-size/size.html">Demo</a> &nbsp;&middot;&nbsp; <a href="https://github.com/eustasy/labs-browser-size">GitHub</a></p>
-					</div>
-				</div>
+<section class="whole grid">
+	<div class="whole smablet-half">
+		<a href="comments-evolved" title="Comments Evolved">
+			<img alt="Comments Evolved" src="assets/images/comments-evolved.png">
+		</a>
+	</div>
+	<div class="whole smablet-half">
+		<h2>Comments Evolved</h2>
+		<p class="text-left">Comments Evolved, or Comments Evolved for Websites, is based on <a href="https://github.com/CloudHeroDevOps/comments-evolved">Comments Evolved for WordPress</a>, and allows for the use of Disqus, Facebook and Google+ comments in a stylish, modular, tabbed format at the bottom of each page.</p>
+		<p><a href="comments-evolved">Demo</a> &nbsp;&middot;&nbsp; <a href="https://github.com/eustasy/comments-evolved">GitHub</a></p>
+	</div>
+</section>
 
+<hr class="section-breaker">
 
+<section class="whole grid">
+	<div class="whole smablet-half float-right">
+		<a href="elementary-Search" title="elementary Search">
+			<img alt="elementary Search" src="assets/images/elementary-Search.png">
+		</a>
+	</div>
+	<div class="whole smablet-half">
+		<h2>elementary Search</h2>
+		<p class="text-left">elementary Search is a small page that allows you to search with Google, Yahoo! or DuckDuckGo in a privacy orientated environment (it uses no Cookies by default) that is both simple to use and beautiful to behold. In the future we hope to have a tighter integration with the elementary project as it moves towards Isis.</p>
+		<p><a href="elementary-Search">Demo</a> &nbsp;&middot;&nbsp; <a href="https://github.com/eustasy/elementary-Search">GitHub</a></p>
+	</div>
+</section>
 
-			</div>
-		</div>
-		<div id="footercontainer">
-			<footer class="section group">
-				<div class="col span_1_of_8">
-					<a href="http://eustasy.org/" title="eustasy organisation"><img class="faded" style="max-width: 48px;" src="http://eustasy.org/favicon.ico"></a>
-				</div>
-				<div class="col span_2_of_8"><br></div>
-				<div class="col span_1_of_8"><p class="box-like center"><a href="http://eustasy.org/legal/cookie-policy" title="Cookie Policy">cookies</a></p></div>
-				<div class="col span_1_of_8"><p class="box-like center"><a href="http://eustasy.org/legal/privacy-policy" title="Privacy Policy">privacy</a></p></div>
-				<div class="col span_1_of_8"><p class="box-like center"><a href="http://eustasy.org/legal/website-disclaimer" title="Website Disclaimer">disclaimer</a></p></div>
-				<div class="col span_1_of_8"><p class="box-like center"><a href="http://eustasy.org/legal/copyright-notice" title="Copyright Notice">copyright</a></p></div>
-				<div class="col span_1_of_8"><p class="box-like center"><a href="http://eustasy.org/legal/terms-of-service" title="Terms Of Service">terms</a></p></div>
-			</footer>
-		</div>
-		<!--[if lt IE 9]>
-			<script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-			<script type="text/javascript">window.jQuery || document.write('<script src="http://labs.eustasy.org/js/jquery-1.10.2.min.js"><\/script>');</script>
-		<![endif]-->
-		<!--[if IE 9]><!-->
-			<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
-			<script>window.jQuery || document.write('<script src="http://labs.eustasy.org/js/jquery-2.0.3.min.js"><\/script>');</script>
-		<!--<![endif]-->
-		<script src="http://labs.eustasy.org/js/modernizr.min.js"></script>
-		<script src="http://labs.eustasy.org/js/jquery.equalize.min.js"></script>
-	</body>
-</html>
+<hr class="section-breaker">
+
+<section class="whole grid">
+	<div class="whole smablet-half">
+		<a href="jQuery.equalize" title="jQuery.equalize">
+			<img alt="jQuery.equalize" src="assets/images/jQuery.equalize.png">
+		</a>
+	</div>
+	<div class="whole smablet-half">
+		<h2>jQuery.equalize</h2>
+		<p class="text-left">jQuery.equalize is a small jQuery plugin to equalize the height of elements with the equalize class within groups, or the entire page. It is open-sourced by eustasy under the MIT License, and the minified version is only 1.8KB in size.</p>
+		<p><a href="jQuery.equalize">Demo</a> &nbsp;&middot;&nbsp; <a href="https://github.com/eustasy/jQuery.equalize">GitHub</a></p>
+	</div>
+</section>
+
+<hr class="section-breaker">
+
+<section class="whole grid">
+	<div class="whole smablet-half float-right">
+		<a href="uptimerobot-status-page" title="Uptime Robot Status Page">
+			<img alt="Uptime Robot Status Page" src="assets/images/uptimerobot-status-page.png">
+		</a>
+	</div>
+	<div class="whole smablet-half">
+		<h2>Uptime Robot Status Page</h2>
+		<p class="text-left">A Status page for Uptime Reports from <a href="http://uptimerobot.com">UptimeRobot.com</a>, making only slight use of their API.</p>
+		<p><a href="uptimerobot-status-page">Demo</a> &nbsp;&middot;&nbsp; <a href="https://github.com/eustasy/uptimerobot-status-page">GitHub</a></p>
+	</div>
+</section>
+
+<hr class="section-breaker">
+
+<section class="whole grid">
+	<div class="whole smablet-half">
+		<a href="browser-size" title="Browser Size">
+			<img alt="Browser Size" src="assets/images/browser-size.png">
+		</a>
+	</div>
+	<div class="whole smablet-half">
+		<h2>Browser Size</h2>
+		<p class="text-left">Browser Size is a small HTML document that relies only on jQuery to quickly and simply display the dimensions of your browser window. This can be especially useful to have locally stored on your machine for manipulating CSS Media Queries and emulating smaller screens.</p>
+		<p><a href="browser-size">Demo</a> &nbsp;&middot;&nbsp; <a href="https://github.com/eustasy/browser-size">GitHub</a></p>
+	</div>
+</section>
+
+<hr class="section-breaker">
+
+<?php include __DIR__.'/_templates/footer.php'; ?>
