77aa61575dfbbf4d157faa42ce4a741e1ac45666
Lewis Goddard <lewisgoddard@eustasy.org>
2016-09-28 21:03:37 +0100

    Add templates, update browser-size

---
+++ _templates/footer.php
@@ -0,0 +1,10 @@
+<footer>
+	<p>
+		<a href="https://github.com/eustasy/labs.eustasy.org/blob/master/LICENSE.md">MIT License</a> &nbsp;&middot;&nbsp;
+		<a href="https://github.com/eustasy/labs.eustasy.org">GitHub</a> &nbsp;&middot;&nbsp;
+		<a href="https://eustasy.org/">eustasy</a>
+	</p>
+</footer>
+
+</body>
+</html>
---
+++ _templates/header.php
@@ -0,0 +1,42 @@
+<!DocType html>
+<html>
+	<head>
+
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+		<title><?php
+		if ( !empty($Page['Title']) ) {
+			echo $Page['Title'].' &nbsp;&middot;&nbsp; ';
+		} ?>eustasy Labs &nbsp;&middot;&nbsp; eustasy Org</title>
+
+		<link rel="icon" href="favicon.ico">
+		<link rel="shortcut icon" href="favicon.ico">
+
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/g/normalize,colors.css">
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300|Lusitana|Roboto:300,300i,400|Source+Code+Pro">
+		<link rel="stylesheet" href="/labs.eustasy.org/assets/css/labs.css">
+
+		<script>
+			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+			ga('create', 'UA-45667989-11', 'eustasy.org');
+			ga('send', 'pageview');
+		</script>
+
+		<script>var jQl={q:[],dq:[],gs:[],ready:function(a){'function'==typeof a&&jQl.q.push(a);return jQl},getScript:function(a,c){jQl.gs.push([a,c])},unq:function(){for(var a=0;a<jQl.q.length;a++)jQl.q[a]();jQl.q=[]},ungs:function(){for(var a=0;a<jQl.gs.length;a++)jQuery.getScript(jQl.gs[a][0],jQl.gs[a][1]);jQl.gs=[]},bId:null,boot:function(a){'undefined'==typeof window.jQuery.fn?jQl.bId||(jQl.bId=setInterval(function(){jQl.boot(a)},25)):(jQl.bId&&clearInterval(jQl.bId),jQl.bId=0,jQl.unqjQdep(),jQl.ungs(),jQuery(jQl.unq()), 'function'==typeof a&&a())},booted:function(){return 0===jQl.bId},loadjQ:function(a,c){setTimeout(function(){var b=document.createElement('script');b.src=a;document.getElementsByTagName('head')[0].appendChild(b)},1);jQl.boot(c)},loadjQdep:function(a){jQl.loadxhr(a,jQl.qdep)},qdep:function(a){a&&('undefined'!==typeof window.jQuery.fn&&!jQl.dq.length?jQl.rs(a):jQl.dq.push(a))},unqjQdep:function(){if('undefined'==typeof window.jQuery.fn)setTimeout(jQl.unqjQdep,50);else{for(var a=0;a<jQl.dq.length;a++)jQl.rs(jQl.dq[a]); jQl.dq=[]}},rs:function(a){var c=document.createElement('script');document.getElementsByTagName('head')[0].appendChild(c);c.text=a},loadxhr:function(a,c){var b;b=jQl.getxo();b.onreadystatechange=function(){4!=b.readyState||200!=b.status||c(b.responseText,a)};try{b.open('GET',a,!0),b.send('')}catch(d){}},getxo:function(){var a=!1;try{a=new XMLHttpRequest}catch(c){for(var b=['MSXML2.XMLHTTP.5.0','MSXML2.XMLHTTP.4.0','MSXML2.XMLHTTP.3.0','MSXML2.XMLHTTP','Microsoft.XMLHTTP'],d=0;d<b.length;++d){try{a= new ActiveXObject(b[d])}catch(e){continue}break}}finally{return a}}};if('undefined'==typeof window.jQuery){var $=jQl.ready,jQuery=$;$.getScript=jQl.getScript};</script>
+		<script>jQl.loadjQ('https://cdn.jsdelivr.net/g/prefixfree,jquery,jquery.equalize,jquery.downboy');</script>
+		<script>
+			$(function(){
+				equalize('.grid', '.equalize');
+				downBoy();
+				window.onresize = function() {
+					equalize('.grid', '.equalize');
+					downBoy();
+				}
+			})
+		</script>
+
+	</head>
